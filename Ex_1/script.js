d1f6bab7db35552b9d2df3d96ffed3d313cfae09
Mohsen Karami <kara0158@algonquinlive.com>
2024-04-24 19:35:52 -0400

    add ex_1

---
+++ Ex_1/script.js
@@ -0,0 +1,13 @@
+const draver = {
+    name: "komod",
+    volume: 23,
+    kesho: 2,
+    dar: 1,
+    color: "green",
+    toll:{
+        right: 12,
+        left: 44,
+    },
+}
+
+console.log(draver.toll.right)
