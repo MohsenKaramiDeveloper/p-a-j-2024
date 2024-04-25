fc201431d49ad1ec03021b927328a0e90b936e82
Mohsen Karami <kara0158@algonquinlive.com>
2024-04-25 15:54:29 -0400

    continue

--- m-43/script.js
+++ m-43/script.js
@@ -15,3 +15,13 @@ console.log(typeof b);
 for(var i =1; i<10; i++){
     console.log('mohsen');
 }
+
+for(var i=1; i<1000; i++){
+    if(i%2==0){
+        continue;
+
+    }
+    console.log(i);
+    
+
+}
