12102ae87215136fa52e6a6d676f2988fe6f9ad4
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-01 20:39:15 -0400

    comment

--- m-43/script.js
+++ m-43/script.js
@@ -1,61 +1,63 @@
-if(1){
-console.log(1)
-};
+// if(1){
+// console.log(1)
+// };
 
-console.log('3'+ true)
-console.log('3'- 3)
+// console.log('3'+ true)
+// console.log('3'- 3)
 
 
-var a = '100';
-console.log(typeof a)
+// var a = '100';
+// console.log(typeof a)
 
-var b = Number(a)
-console.log(typeof b);
+// var b = Number(a)
+// console.log(typeof b);
 
-for(var i =1; i<10; i++){
-    console.log('mohsen');
-}
+// for(var i =1; i<10; i++){
+//     console.log('mohsen');
+// }
 
-for(var i=1; i<1000; i++){
-    if(i%2==0){
-        continue;
+// for(var i=1; i<1000; i++){
+//     if(i%2==0){
+//         continue;
 
-    }
-    console.log(i);
+//     }
+//     console.log(i);
     
 
-}
+// }
 
-console.log('3'+ true)
-console.log('3'- '3')
+// console.log('3'+ true)
+// console.log('3'- '3')
 
-// mm
+// // mm
 
-for(var t = 1; t<=2; t++){
-    console.log('outer' + b++)
-    {
-        console.log('inner'+b)
-    }
-}
-
-
-var array1 = [1, true, "mohsen", false, 123];
-console.log(array1);
-let x = array1.toString();
-console.log(x);
-console.log(array1.join('    @    '));
-var w = array1.pop();
-console.log(array1.join('    @    '));
-console.log(w);
+// for(var t = 1; t<=2; t++){
+//     console.log('outer' + b++)
+//     {
+//         console.log('inner'+b)
+//     }
+// }
 
 
+// var array1 = [1, true, "mohsen", false, 123];
+// console.log(array1);
+// let x = array1.toString();
+// console.log(x);
+// console.log(array1.join('    @    '));
+// var w = array1.pop();
+// console.log(array1.join('    @    '));
+// console.log(w);
 
-let arr1 = [1,2,3,4]
 
-function add1(){
 
-}
+// let arr1 = [1,2,3,4]
 
-let add2 = function(){
-    
-}
+// function add1(){
+
+// }
+
+// let add2 = function(){
+
+// }
+
+
