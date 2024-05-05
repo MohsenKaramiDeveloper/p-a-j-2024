d96d244bd478ee806963fae2f3515de8da2f973b
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-04 23:55:02 -0400

    console.log(typeof a)

--- m-43/script.js
+++ m-43/script.js
@@ -6,15 +6,15 @@
 // console.log('3'- 3)
 
 
-// var a = '100';
-// console.log(typeof a)
+var a = '100';
+console.log(typeof a)
 
-// var b = Number(a)
-// console.log(typeof b);
+var b = Number(a)
+console.log(typeof b);
 
-// for(var i =1; i<10; i++){
-//     console.log('mohsen');
-// }
+for(var i =1; i<10; i++){
+    console.log('mohsen');
+}
 
 for(var i=1; i<1000; i++){
     if(i%2==0){
