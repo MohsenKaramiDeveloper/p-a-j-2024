41b449b8f35b1742398b26d1a494d1238f1feb8c
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-04 23:53:37 -0400

    array1.pop

--- m-43/script.js
+++ m-43/script.js
@@ -39,14 +39,14 @@
 // }
 
 
-// var array1 = [1, true, "mohsen", false, 123];
-// console.log(array1);
-// let x = array1.toString();
-// console.log(x);
-// console.log(array1.join('    @    '));
-// var w = array1.pop();
-// console.log(array1.join('    @    '));
-// console.log(w);
+var array1 = [1, true, "mohsen", false, 123];
+console.log(array1);
+let x = array1.toString();
+console.log(x);
+console.log(array1.join('    @    '));
+var w = array1.pop();
+console.log(array1.join('    @    '));
+console.log(w);
 
 
 
