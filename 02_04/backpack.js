2efe3ee055dc05e633c5d7170d3056450d1f2776
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-08 23:52:26 -0400

    let main = document.querySelector("main");

--- 02_04/backpack.js
+++ 02_04/backpack.js
@@ -1,5 +1,5 @@
 const updateBackpack = (update) => {
-  let main = document.querySelector("main");
+  // let main = document.querySelector("main");
   main.innerHTML = markup(backpack);
   console.info(update);
 };
