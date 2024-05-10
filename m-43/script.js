e54f39ce9f8eaad9f818e469b9abca4b07accf27
Mohsen Karami <kara0158@algonquinlive.com>
2024-05-09 23:15:34 -0400

    var w

--- m-43/script.js
+++ m-43/script.js
@@ -44,7 +44,7 @@ console.log(array1);
 let x = array1.toString();
 console.log(x);
 console.log(array1.join('    @    '));
-// var w = array1.pop();
+var w = array1.pop();
 console.log(array1.join('    @    '));
 console.log(w);
 
